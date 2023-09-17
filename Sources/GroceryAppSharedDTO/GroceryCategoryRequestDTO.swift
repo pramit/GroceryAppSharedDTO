cac74c2f225cbbd17ec1c28faf2835ff51828962
pramit <pramit.rashinkar@gmail.com>
2023-09-17 14:04:02 -0700

    Added GroceryCategoryRequestDTO and GroceryCategoryResponseDTO

---
+++ Sources/GroceryAppSharedDTO/GroceryCategoryRequestDTO.swift
@@ -0,0 +1,20 @@
+//
+//  File.swift
+//  
+//
+//  Created by Pramit Rashinkar on 9/17/23.
+//
+
+import Foundation
+
+public struct GroceryCategoryRequestDTO: Codable {
+    
+    public let title: String
+    public let colorCode: String
+    
+    public init(title: String, colorCode: String) {
+        self.title = title
+        self.colorCode = colorCode
+    }
+    
+}
---
+++ Sources/GroceryAppSharedDTO/GroceryCategoryResponseDTO.swift
@@ -0,0 +1,22 @@
+//
+//  File.swift
+//  
+//
+//  Created by Pramit Rashinkar on 9/17/23.
+//
+
+import Foundation
+
+public struct GroceryCategoryResponseDTO: Codable {
+    
+    public let id: UUID
+    public let title: String
+    public let colorCode: String
+    
+    public init(id: UUID, title: String, colorCode: String) {
+        self.id = id
+        self.title = title
+        self.colorCode = colorCode
+    }
+    
+}
